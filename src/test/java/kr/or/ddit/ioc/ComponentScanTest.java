abff9ce21997da958bd04cb5101225ffb488e343
GunHoKim <kgh3680@gmail.com>
2021-01-29 10:51:00 +0900

    프레임워크와 라이브러리 스프링 프레임워크의 탄생 배경
    IOC 컨테이너
    
    IOC 컨테이너 생성
     - sts3 플러그인 설치
     - 스프링 설정파일(스프링 빈 생성) 작성
     - scope 테스트(singleton, prototype)
     - collection 객체 생성
     - property-placeholder
     - java config

---
+++ src/test/java/kr/or/ddit/ioc/ComponentScanTest.java
@@ -0,0 +1,42 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.user.model.UserVo;
+import kr.or.ddit.user.repository.UserDao;
+import kr.or.ddit.user.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/kr/or/ddit/ioc/conponent-scan.xml")
+public class ComponentScanTest {
+
+	@Resource(name="userDao")
+	private UserDao userDao;
+	
+	@Resource(name="userServiceImpl")
+	private UserService userService;
+	
+	//@Repository 어노페이션을 적용한 UserDaoImpl 스프링 빈이 정상적으로 컨테이너에 등록 되었는지 확인
+	@Test
+	public void UserDaoImplSpringBeanTest() {
+		assertNotNull(userDao);
+		UserVo userVo = userDao.getUser("brown");
+		assertEquals("브라운", userVo.getUsernm());
+	}
+	
+	//userServiceImpl 스프링 빈이 정상적으로 컨테이너에 등록 되었는지 확인
+	@Test
+	public void UserServiceImplSpringBeanTest() {
+		assertNotNull(userDao);
+		UserVo userVo = userService.getUser("brown");
+		assertEquals("브라운", userVo.getUsernm());
+	}
+
+}
